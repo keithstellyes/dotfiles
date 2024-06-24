7fb79b062117bd16d554477953a33d7a36c7abda
Keith Stellyes <keithastellyes@gmail.com>
2024-06-23 19:43:02 -0700

    Install CJK fonts for Arch

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ function install() {
 }
 
 install git neovim tmux gcc pwgen
+# arch by default won't have cjk supoprt
+uname -r | grep arch && install noto-fonts-cjk
 cp .profile.local ~/
 cp .profile.shared ~/
 mkdir -p ~/.config/nvim/
